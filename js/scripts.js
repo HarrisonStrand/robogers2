c15d80b339b58c4351466fdce8e657f98f5a68cd
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-07 13:28:57 -0800

    start of .map to new array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
+// function beepBoop(number) {
+//   let numArray = [];
+//   for(let i = 1; i <= number; i +=1) {
+//     if(i == ("3")) {
+//       numArray.push("Won't You Be My Neighbor?");
+//     } else if (i == ("2")) {
+//       numArray.push("Boop!");
+//     } else if (i == ("1")) {
+//       numArray.push("Beep!");
+//     } else {
+//       numArray.push(i);
+//     }
+//   }
+
+//   return numArray.join(", ");
+// }
 function beepBoop(number) {
   let numArray = [];
   for(let i = 1; i <= number; i +=1) {
-    if(i == (3)) {
-      numArray.push("Won't You Be My Neighbor?");
-    } else if (i == (2)) {
-      numArray.push("Boop!");
-    } else if (i == (1)) {
-      numArray.push("Beep!");
-    } else {
-      numArray.push(i);
-    }
+    numArray.push(i);
   }
-  return numArray.join(" ");
 }
 
+return numArray.join(", ");
 
+
+if(i == ("3")) {
+  numArray.push("Won't You Be My Neighbor?");
+} else if (i == ("2")) {
+  numArray.push("Boop!");
+} else if (i == ("1")) {
+  numArray.push("Beep!");
+} else {
 $(document).ready(function() {
   $("button#submit").click(function(event) {
     event.preventDefault();
